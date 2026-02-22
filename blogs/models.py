6fe3f3516e2141f95dc7d287762c9180752c17fc
AmanPatel555 <23bcs001@gmail.com>
2026-02-22 21:17:42 +0530

    Added New Features

--- blogs/models.py
+++ blogs/models.py
@@ -1,10 +1,5 @@
 from django.db import models
 
-# Create your models here.
-from django.db import models
-
-# Create your models here.
-
 class Author(models.Model):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
@@ -35,3 +30,11 @@ class Post(models.Model):
     def __str__(self):
         return self.title
 
+class Comment(models.Model):
+    user_name = models.CharField(max_length=25)
+    user_email = models.EmailField()
+    comment_text = models.TextField(max_length=400)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+
+    def __str__(self):
+        return self.comment_text
